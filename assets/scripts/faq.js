ded676b05848271d78f79b8966c60805d65a0ace
Xavier Brigode <xavier.brigode@gmail.com>
2022-10-24 16:49:14 +0200

    add js features on faq section, mobile design rework > Mobile design done

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -1,36 +1,76 @@
-const arrow1 = document.querySelector(".faq-button1");
-const arrow2 = document.querySelector(".faq-button2");
-const arrow3 = document.querySelector(".faq-button3");
-const arrow4 = document.querySelector(".faq-button4");
+const question1 = document.querySelector(".q1-question");
+const question2 = document.querySelector(".q2-question");
+const question3 = document.querySelector(".q3-question");
+const question4 = document.querySelector(".q4-question");
 
 const response1 = document.querySelector(".q1-response");
 const response2 = document.querySelector(".q2-response");
 const response3 = document.querySelector(".q3-response");
 const response4 = document.querySelector(".q4-response");
 
+const arrow1 = document.querySelector(".faq-arrow1")
+const arrow2 = document.querySelector(".faq-arrow2")
+const arrow3 = document.querySelector(".faq-arrow3")
+const arrow4 = document.querySelector(".faq-arrow4")
 
 
-arrow1.onclick = function () {
-    response1.style.display = "inline-block";
+question1.addEventListener ("click", function() {
+    if (response1.style.display === "inline-block") {
+        response1.style.display = "none";
+        arrow1.style.rotate = "0deg"
+    }else if(response1.style.display === "none"){
+        response1.style.display = "inline-block";
+        arrow1.style.rotate = "-180deg"
+    }
     response2.style.display = "none";
     response3.style.display = "none";
     response4.style.display = "none";
-};
-arrow2.onclick = function () {
-    response2.style.display = "inline-block";
+    arrow2.style.rotate = "0deg"
+    arrow3.style.rotate = "0deg"
+    arrow4.style.rotate = "0deg"
+});
+question2.onclick = function () {
+    if (response2.style.display === "inline-block") {
+        response2.style.display = "none";
+        arrow2.style.rotate = "0deg"
+    }else if(response2.style.display === "none"){
+        response2.style.display = "inline-block";
+        arrow2.style.rotate = "-180deg"
+    }
     response1.style.display = "none";
     response3.style.display = "none";
     response4.style.display = "none";
+    arrow1.style.rotate = "0deg"
+    arrow3.style.rotate = "0deg"
+    arrow4.style.rotate = "0deg"
 };
-arrow3.onclick = function () {
-    response3.style.display = "inline-block";
+question3.onclick = function () {
+    if (response3.style.display === "inline-block") {
+        response3.style.display = "none";
+        arrow3.style.rotate = "0deg"
+    }else if(response3.style.display === "none"){
+        response3.style.display = "inline-block";
+        arrow3.style.rotate = "-180deg"
+    }
     response2.style.display = "none";
     response1.style.display = "none";
     response4.style.display = "none";
+    arrow2.style.rotate = "0deg"
+    arrow1.style.rotate = "0deg"
+    arrow4.style.rotate = "0deg"
 };
-arrow4.onclick = function () {
-    response4.style.display = "inline-block";
+question4.onclick = function () {
+    if (response4.style.display === "inline-block") {
+        response4.style.display = "none";
+        arrow4.style.rotate = "0deg"
+    }else if(response4.style.display === "none"){
+        response4.style.display = "inline-block";
+        arrow4.style.rotate = "-180deg"
+    }
     response2.style.display = "none";
     response3.style.display = "none";
     response1.style.display = "none";
+    arrow2.style.rotate = "0deg"
+    arrow3.style.rotate = "0deg"
+    arrow1.style.rotate = "0deg"
 };
