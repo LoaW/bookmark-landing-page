1a085a8fcbe203cf39b93f9d35d1be808d45184b
Xavier Brigode <xavier.brigode@gmail.com>
2022-11-01 21:20:27 +0100

    refactor: faq bahvior

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -1,79 +1,17 @@
-const question1 = document.querySelector(".q1-question");
-const question2 = document.querySelector(".q2-question");
-const question3 = document.querySelector(".q3-question");
-const question4 = document.querySelector(".q4-question");
-
-const response1 = document.querySelector(".q1-response");
-const response2 = document.querySelector(".q2-response");
-const response3 = document.querySelector(".q3-response");
-const response4 = document.querySelector(".q4-response");
-
-const arrow1 = document.querySelector(".faq-arrow1")
-const arrow2 = document.querySelector(".faq-arrow2")
-const arrow3 = document.querySelector(".faq-arrow3")
-const arrow4 = document.querySelector(".faq-arrow4")
-
-
-question1.onclick = () => {
-    if (response1.style.display === "inline-block") {
-        response1.style.display = "";
-        arrow1.style.rotate = "0deg";
-        console.log("test if")
-    }else if(response1.style.display === ""){
-        response1.style.display = "inline-block";
-        arrow1.style.rotate = "-180deg"
-        console.log("test else if")
-    }
-    console.log(response1.style.display === "inline-block")
-    response2.style.display = "";
-    response3.style.display = "";
-    response4.style.display = "";
-    arrow2.style.rotate = "0deg"
-    arrow3.style.rotate = "0deg"
-    arrow4.style.rotate = "0deg"
-};
-question2.onclick = function () {
-    if (response2.style.display === "inline-block") {
-        response2.style.display = "";
-        arrow2.style.rotate = "0deg"
-    }else if(response2.style.display === ""){
-        response2.style.display = "inline-block";
-        arrow2.style.rotate = "-180deg"
-    }
-    response1.style.display = "";
-    response3.style.display = "";
-    response4.style.display = "";
-    arrow1.style.rotate = "0deg"
-    arrow3.style.rotate = "0deg"
-    arrow4.style.rotate = "0deg"
-};
-question3.onclick = function () {
-    if (response3.style.display === "inline-block") {
-        response3.style.display = "";
-        arrow3.style.rotate = "0deg"
-    }else if(response3.style.display === ""){
-        response3.style.display = "inline-block";
-        arrow3.style.rotate = "-180deg"
-    }
-    response2.style.display = "";
-    response1.style.display = "";
-    response4.style.display = "";
-    arrow2.style.rotate = "0deg"
-    arrow1.style.rotate = "0deg"
-    arrow4.style.rotate = "0deg"
-};
-question4.onclick = function () {
-    if (response4.style.display === "inline-block") {
-        response4.style.display = "";
-        arrow4.style.rotate = "0deg"
-    }else if(response4.style.display === ""){
-        response4.style.display = "inline-block";
-        arrow4.style.rotate = "-180deg"
-    }
-    response2.style.display = "";
-    response3.style.display = "";
-    response1.style.display = "";
-    arrow2.style.rotate = "0deg"
-    arrow3.style.rotate = "0deg"
-    arrow1.style.rotate = "0deg"
-};
+const questions = document.querySelectorAll(".question");
+console.log(questions);
+for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", () => {
+        const response = document.querySelector(`.q${i + 1}-response`);
+        const arrow = document.querySelector(`.faq-arrow${i + 1}`);
+        if (response.style.display == "") {
+            response.style.display = "flex";
+            arrow.style.rotate = "180deg"
+            arrow.style.filter = "invert(49%) sepia(56%) saturate(5585%) hue-rotate(334deg) brightness(127%) contrast(116%)"
+        } else if (response.style.display == "flex") {
+            response.style.display = "";
+            arrow.style.rotate = "0deg"
+            arrow.style.filter = "invert(44%) sepia(90%) saturate(3186%) hue-rotate(217deg) brightness(91%) contrast(92%)"
+        }
+    });
+}
--- assets/scripts/slide.js
+++ assets/scripts/slide.js
@@ -4,33 +4,31 @@ const speedyButton = document.querySelector(".searching");
 const speedySlide = document.querySelector(".s2");
 const easyButton = document.querySelector(".sharing");
 const easySlide = document.querySelector(".s3");
-const book_Highlight = document.querySelector(".bookmarking__highlight")
-const sear_Highlight = document.querySelector(".searching__highlight")
-const shar_Highlight = document.querySelector(".sharing__highlight")
+
 
 simpleButton.onclick = function () {
     simpleSlide.style.display = "flex";
     speedySlide.style.display = "none";
     easySlide.style.display = "none";
-    book_Highlight.style.borderBottom = "4px solid hsl(0, 94%, 66%)";
-    sear_Highlight.style.borderBottom = "none";
-    shar_Highlight.style.borderBottom = "none";
+    simpleButton.style.borderBottom = "4px solid hsl(0, 94%, 66%)";
+    speedyButton.style.borderBottom = "0.5px solid rgb(199, 199, 199)";
+    easyButton.style.borderBottom = "0.5px solid rgb(199, 199, 199)";
 };
 
 speedyButton.onclick = function () {
     simpleSlide.style.display = "none";
     speedySlide.style.display = "flex";
     easySlide.style.display = "none";
-    book_Highlight.style.borderBottom = "none";
-    sear_Highlight.style.borderBottom = "4px solid hsl(0, 94%, 66%)";
-    shar_Highlight.style.borderBottom = "none";
+    simpleButton.style.borderBottom = "0.5px solid rgb(199, 199, 199)";
+    speedyButton.style.borderBottom = "4px solid hsl(0, 94%, 66%)";
+    easyButton.style.borderBottom = "0.5px solid rgb(199, 199, 199)";
 };
 
 easyButton.onclick = function () {
     simpleSlide.style.display = "none";
     speedySlide.style.display = "none";
     easySlide.style.display = "flex";
-    book_Highlight.style.borderBottom = "none";
-    sear_Highlight.style.borderBottom = "none";
-    shar_Highlight.style.borderBottom = "4px solid hsl(0, 94%, 66%)";
+    simpleButton.style.borderBottom = "0.5px solid rgb(199, 199, 199)";
+    speedyButton.style.borderBottom = "0.5px solid rgb(199, 199, 199)";
+    easyButton.style.borderBottom = "4px solid hsl(0, 94%, 66%)";
 };
